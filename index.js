cc4b1d212370dff269dfa5efedf8fb8fe9bbb17b
Victor Nogueira <69355100+victorftrdba@users.noreply.github.com>
2022-10-25 23:16:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const products = [{
     id: 10, name: 'Chinelo', price: 125.00, category: 'Roupas'
 }, {
     id: 11, name: 'PS5', price: 2500.00, category: 'Video games'
+}, {
+    id: 15, name: 'Computador Gamer', price: 3685.12, category: 'Eletrônicos'
 }]
 
 const btnSubmit = document.getElementById('btnSubmit')
@@ -13,6 +15,7 @@ function initCart() {
         document.getElementById('cart').innerHTML += `
             <div>Produto ${index} - ${element.name}</div>
             Quantidade: <input type="number" name="${element.id}" />
+            <div>Valor do produto: ${parseFloat(element.price).toFixed(2)}</div>
         `
     })
 }
@@ -26,14 +29,14 @@ function submit(e) {
         const product = products.find(val => val.id == index)
         if (quantity > 0) {
             total += product.price * quantity
-            productsBought.push({name: product.name})
+            productsBought.push({name: product.name, quantity})
         }
     })
-    document.getElementById('totalOrder').innerHTML = parseFloat(total).toFixed(2)
+    document.getElementById('totalOrder').innerHTML = `Total do pedido: ${parseFloat(total).toFixed(2)}`
     console.log('Parabéns pelo seu pedido!')
     console.log({
         productsBought,
-        total
+        total: parseFloat(total).toFixed(2)
     })
 }
 
